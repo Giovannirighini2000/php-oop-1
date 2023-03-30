2a74b795d182d2ead0ed01fda1642a429eefa4ea
Giovannirighini2000 <righinigiovanni0@gmail.com>
2023-03-30 17:16:16 +0200

    feat added method added h,m on duration

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
  $movie1 ->type ="thriller, fantastico, noir";
 
  var_dump($movie1);
-
+ var_dump($movie1->MoviesDurations->hours . "h " . $movie1->MoviesDurations->minutes . "m");
 //  $duration1 = new MoviesDurations("1","57");
 
 //  var_dump($duration1);
@@ -21,7 +21,7 @@
 //  $movie2 ->infoDirectorName ="Sydney Sibilia";
  $movie2 ->type ="commedia, drammatico, storico";
  var_dump($movie2);
-
+ var_dump($movie2->MoviesDurations->hours . "h " . $movie2->MoviesDurations->minutes . "m");
 //  $duration2 = new MoviesDurations("2","27");
 
 //  var_dump($duration2);
